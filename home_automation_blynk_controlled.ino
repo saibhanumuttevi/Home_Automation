e19ed58c1f615e99dc7acb08232be37ad35cb0de
Sai Bhanu <83135144+saibhanumuttevi@users.noreply.github.com>
2022-09-07 00:05:51 +0530

    Update home_automation_blynk_controlled.ino

--- home_automation_blynk_controlled.ino
+++ home_automation_blynk_controlled.ino
@@ -1,8 +1,8 @@
 // Template ID, Device Name and Auth Token are provided by the Blynk.Cloud
 // See the Device Info tab, or Template settings
-#define BLYNK_TEMPLATE_ID "TMPLK-FSX1SQ"
+#define BLYNK_TEMPLATE_ID " "
 #define BLYNK_DEVICE_NAME "Home Automation"
-#define BLYNK_AUTH_TOKEN "iAxhK7cOREEVW2D0OvoDxT9_jluzl391"
+#define BLYNK_AUTH_TOKEN " "
 
 
 /*************************************************************
